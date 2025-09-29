48e849f6c9a6f35bd90e50f70d3f94f2790a8900
nabilsiddik <nabilsiddik90@gmail.com>
2025-09-30 05:06:21 +0600

    build

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 // middlewares
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: 'https://b5-a7-portfolio-front-end.vercel.app',
     credentials: true
 }));
 app.use(express_1.default.json());
