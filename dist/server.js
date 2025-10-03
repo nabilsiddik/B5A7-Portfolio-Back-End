c39ef88ee9869ea28cd46973246c35d6e8e3abf7
nabilsiddik <nabilsiddik90@gmail.com>
2025-10-03 12:43:27 +0600

    deploy

--- dist/server.js
+++ dist/server.js
@@ -13,9 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-const app_1 = __importDefault(require("./app"));
 const db_config_1 = require("./app/config/db.config");
-const seedAdmin_1 = require("./app/utils/seedAdmin");
 dotenv_1.default.config();
 const port = process.env.PORT || 5000;
 const dbConnect = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -30,10 +28,10 @@ const dbConnect = () => __awaiter(void 0, void 0, void 0, function* () {
 });
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     yield dbConnect();
-    app_1.default.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
-        console.log(`Server is running on port ${port}`);
-        yield (0, seedAdmin_1.seedAdmin)();
-    }));
+    // app.listen(port, async () => {
+    //   console.log(`Server is running on port ${port}`);
+    //   await seedAdmin();
+    // });
 });
 (() => __awaiter(void 0, void 0, void 0, function* () {
     yield startServer();
